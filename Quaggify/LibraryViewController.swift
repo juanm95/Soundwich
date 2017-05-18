94e93dbae361c2daa63f9b17fd2fc05a28bbbdc4
Juan Marroquin <juam95@stanford.edu>
2017-05-17 19:47:16 -0700

    Attach text to reaction

--- Quaggify/LibraryViewController.swift
+++ Quaggify/LibraryViewController.swift
@@ -13,7 +13,8 @@ import Flurry_iOS_SDK
 
 class LibraryViewController: ViewController, SPTAudioStreamingDelegate, SPTAudioStreamingPlaybackDelegate {
     
-    private func react(reaction: String, trackResponse: Track, tomember: String, time: String) {
+    private func react(reactionEmoji: String, trackResponse: Track, tomember: String, time: String, elaboration: String) {
+        let reaction = "\(reactionEmoji) \(elaboration)"
         API.reactToSong(reaction: reaction, time: time, username: UserDefaults.standard.value(forKey: "username") as! String, to: tomember)
         thePlayer.needToReact = false
         thePlayer.injected = false
@@ -35,7 +36,7 @@ class LibraryViewController: ViewController, SPTAudioStreamingDelegate, SPTAudio
         API.addTrackToPlaylist(track: trackResponse, playlist: soundwichPlaylist) {(string: String?, error: Error?) in
             print (string)
         }
-        let reactionParameters = ["reaction": reaction] as [String: Any]
+        let reactionParameters = ["reaction": reactionEmoji, "message": elaboration] as [String: Any]
         Flurry.endTimedEvent("Reacting", withParameters: reactionParameters)
     }
     
@@ -84,17 +85,24 @@ class LibraryViewController: ViewController, SPTAudioStreamingDelegate, SPTAudio
                     } else if let trackResponse = trackResponse {
                         let alertController = UIAlertController(title: "Soundwich", message: "React to this song \(frommember) sent.", preferredStyle: UIAlertControllerStyle.alert)
                         alertController.addAction(UIAlertAction(title: "💩", style: UIAlertActionStyle.default, handler: {[weak self] (alert: UIAlertAction!) in
-                            self?.react(reaction: "💩", trackResponse: trackResponse, tomember: tomember, time: time)
+                            let elaboration = alertController.textFields![0].text!
+                            self?.react(reactionEmoji: "💩", trackResponse: trackResponse, tomember: tomember, time: time, elaboration: elaboration)
                         }))
                         alertController.addAction(UIAlertAction(title: "😂", style: UIAlertActionStyle.default, handler: {(alert: UIAlertAction!) in
-                            self?.react(reaction: "😂", trackResponse: trackResponse, tomember: tomember, time: time)
+                            let elaboration = alertController.textFields![0].text!
+                            self?.react(reactionEmoji: "😂", trackResponse: trackResponse, tomember: tomember, time: time, elaboration: elaboration)
                         }))
                         alertController.addAction(UIAlertAction(title: "😡", style: UIAlertActionStyle.default, handler: {[weak self] (alert: UIAlertAction!) in
-                            self?.react(reaction: "😡", trackResponse: trackResponse, tomember: tomember, time: time)
+                            let elaboration = alertController.textFields![0].text!
+                            self?.react(reactionEmoji: "😡", trackResponse: trackResponse, tomember: tomember, time: time, elaboration: elaboration)
                         }))
                         alertController.addAction(UIAlertAction(title: "😎", style: UIAlertActionStyle.default, handler: {[weak self] (alert: UIAlertAction!) in
-                            self?.react(reaction: "😎", trackResponse: trackResponse, tomember: tomember, time: time)
+                            let elaboration = alertController.textFields![0].text!
+                            self?.react(reactionEmoji: "😎", trackResponse: trackResponse, tomember: tomember, time: time, elaboration: elaboration)
                         }))
+                        alertController.addTextField { textfield in
+                            textfield.placeholder = "Attach message to reaction"
+                        }
                         thePlayer.nowPlaying?.track = trackResponse
                         DispatchQueue.main.async {
                             UIApplication.topViewController()?.present(alertController, animated: true)
